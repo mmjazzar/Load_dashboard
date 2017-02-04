7f627f175809ef594e069aae79c369d994f0fe27
Maged M. Eljazzar <eng.maged91@hotmail.com>
2017-02-04 20:43:51 +0200

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,1 +1,35 @@
 
+
+
+library(shiny)
+sidebar <- dashboardSidebar(
+ 
+        fileInput('file1', 'Choose CSV File',
+                  accept=c('text/csv', 
+                           'text/comma-separated-values,text/plain', 
+                           '.csv')),
+        tags$hr(),
+        checkboxInput('header', 'Header', TRUE),
+        radioButtons('sep', 'Separator',
+                     c(Comma=',',
+                       Semicolon=';',
+                       Tab='\t'),
+                     ','),
+        radioButtons('quote', 'Quote',
+                     c(None='',
+                       'Double Quote'='"',
+                       'Single Quote'="'"),
+                     '"')
+      )
+
+body <- dashboardBody(
+  tableOutput('contents')
+  
+)
+
+# Put them together into a dashboardPage
+dashboardPage(
+  dashboardHeader(title = "Simple tabs"),
+  sidebar,
+  body
+)
