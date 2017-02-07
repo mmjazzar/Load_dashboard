acd0cc8b504c758d6705bf8cc504d12cd6fcb2b6
Maged M. Eljazzar <eng.maged91@hotmail.com>
2017-02-08 01:35:00 +0200

    Update Server.r
    adding save button

--- Server.r
+++ Server.r
@@ -1,26 +1,51 @@
-
-
-
 library(shiny)
+library(ggplot2)
 
-function(input, output) {
-  output$contents <- renderTable({
+function(input, output, session) {
+  # added "session" because updateSelectInput requires it
+  
+  data <- reactive({ 
+    req(input$file1) ## ?req #  require that the input is available
     
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, it will be a data frame with 'name',
-    # 'size', 'type', and 'datapath' columns. The 'datapath'
-    # column will contain the local filenames where the data can
-    # be found.
+    inFile <- input$file1 
     
-    inFile <- input$file1
+    df <- read.csv(inFile$datapath, header = TRUE, sep = input$sep)
     
-    if (is.null(inFile))
-      return(NULL)
+    # Update inputs (you could create an observer with both updateSel...)
+    # You can also constraint your choices. If you wanted select only numeric
+    # variables you could set "choices = sapply(df, is.numeric)"
+    # It depends on what do you want to do later on.
     
-    read.csv(inFile$datapath, header=input$header, sep=input$sep, 
-             quote=input$quote)
+    updateSelectInput(session, inputId = 'xcol', label = 'X Variable',
+                      choices = names(df), selected = names(df))
+    updateSelectInput(session, inputId = 'ycol', label = 'Y Variable',
+                      choices = names(df), selected = names(df)[2])
+    
+    return(df)
   })
+  
+  output$contents <- renderTable({
+    data()
+  })
+  
+  # I Since you have two inputs I decided to make a scatterplot
+    output$MyPlot <- renderPlot({
+      x <- data()[, c(input$xcol, input$ycol)]
+    plot(x)
+      
+    })
+    #download plot downloadPlot
+    output$downloadPlot <- downloadHandler(
+      filename <- function() {
+        paste('plot1', 'png', sep = ".")
+      },
+      content <- function(file) {
+        png(file)
+        x <- data()[, c(input$xcol, input$ycol)]
+        plot(x)
+      dev.off()
+      },
+      contentType = "image/png"
+    )
+    
 }
-
-
-
