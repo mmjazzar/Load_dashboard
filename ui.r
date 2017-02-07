a8f7701bdfd4317025700427bbbb2d494ab97ebd
Maged M. Eljazzar <eng.maged91@hotmail.com>
2017-02-08 01:35:24 +0200

    Update ui.r
    adding save button

--- ui.r
+++ ui.r
@@ -1,35 +1,55 @@
-
-
-
 library(shiny)
-sidebar <- dashboardSidebar(
- 
-        fileInput('file1', 'Choose CSV File',
-                  accept=c('text/csv', 
-                           'text/comma-separated-values,text/plain', 
-                           '.csv')),
-        tags$hr(),
-        checkboxInput('header', 'Header', TRUE),
-        radioButtons('sep', 'Separator',
-                     c(Comma=',',
-                       Semicolon=';',
-                       Tab='\t'),
-                     ','),
-        radioButtons('quote', 'Quote',
-                     c(None='',
-                       'Double Quote'='"',
-                       'Single Quote'="'"),
-                     '"')
-      )
+library(datasets)
 
-body <- dashboardBody(
-  tableOutput('contents')
-  
-)
-
-# Put them together into a dashboardPage
-dashboardPage(
-  dashboardHeader(title = "Simple tabs"),
-  sidebar,
-  body
+ui <- shinyUI(fluidPage(
+  titlePanel("Load Forecasting Dashboard"),
+  tabsetPanel(
+    tabPanel("Upload File",
+             titlePanel("Uploading Files"),
+             sidebarLayout(
+               sidebarPanel(
+                 fileInput('file1', 'Choose CSV File',
+                    accept=c('text/csv', 
+                      'text/comma-separated-values,text/plain','.csv')),
+                 
+                radioButtons('sep', 'Separator',c(Comma=','
+                    ,Semicolon=';',Tab='\t'),',')
+              ),
+               
+               mainPanel(
+                 tableOutput('contents')
+               )
+             )#sidebar layout
+    ),
+    tabPanel("plot",
+             pageWithSidebar(
+               headerPanel('Data'),
+               sidebarPanel(
+                 
+                 # "Empty inputs" - they will be updated after the data is uploaded
+                 selectInput('xcol', 'X axis', ""),
+                 selectInput('ycol', 'Y axis', "", selected = ""),
+                 downloadButton('downloadPlot', 'Download Plot')
+               ),
+               mainPanel(
+                 plotOutput('MyPlot')
+               )
+             )
+    ),# end of tabpanel 2
+    tabPanel("Model",
+             pageWithSidebar(
+               headerPanel('Model'),
+               sidebarPanel(
+                 
+                 # "Empty inputs" - they will be updated after the data is uploaded
+               
+               ),#main panel of tabpanel 2
+               mainPanel( 
+               
+               )
+             )
+    )
+    
+  )#end tabset panel
+)#end fluidpanel
 )
