0dcb6f9ba9d0162d45bdd08c85e4d9466a7fe38f
Maged M. Eljazzar <eng.maged91@hotmail.com>
2017-02-14 15:18:54 +0200

    Update ui.r

--- ui.r
+++ ui.r
@@ -21,6 +21,8 @@ ui <- shinyUI(fluidPage(
                )
              )#sidebar layout
     ),
+    ########################################
+    #########################################
     tabPanel("plot",
              pageWithSidebar(
                headerPanel('Data'),
@@ -36,19 +38,99 @@ ui <- shinyUI(fluidPage(
                )
              )
     ),# end of tabpanel 2
+    #####################################################################
+    ####################################################################
     tabPanel("Model",
              pageWithSidebar(
                headerPanel('Model'),
-               sidebarPanel(
-                 
-                 # "Empty inputs" - they will be updated after the data is uploaded
+               # 1. upload button for regressors. 
+               # 2. apply a specific model.
+               # 3. divide the data.
+                sidebarPanel(
+                selectInput("model", "please specifiy the model",
+                             c("ARIMA" = "ARIMA",
+                               "ANN" = "ANN"
+                                  
+               )),  selectInput("freq", "please specifiy the data frequency",
+                                c("24" = "24",
+                                  "168" = "168",
+                                  "365" = "365",
+                                  "7866" = "7866"
+                                )),
+               sliderInput(    inputId   = "train",
+                               label     ="Training",
+                               min       = 1,
+                               max       = 70, 
+                               value     = 70),
+               sliderInput(    inputId   = "test",
+                               label     = "Testing",
+                               min       = 1,  
+                               max       = 30,  
+                               value     = 15),
+               sliderInput(    inputId   = "Validte",
+                               label     ="Validation",
+                               min       = 1,
+                               max       = 30, 
+                               value     = 15)
                
-               ),#main panel of tabpanel 2
+               ),
+               #main panel of tabpanel 3
                mainPanel( 
-               
+                
+                 textOutput('modelData')
                )
              )
+    ),
+    #end of main panel 3 ####################################
+    ##########################################################
+    
+    tabPanel("Forecast Plot",
+             titlePanel("Forecasting Polt"),
+             sidebarLayout(
+               sidebarPanel(
+                 
+                 
+               ),
+               
+               mainPanel(
+                 
+               )
+             )#sidebar layout
+    ),
+    #end of main panel 4 ####################################
+    ##########################################################
+    tabPanel("Forecasting Data",
+             titlePanel("Forecasting Data"),
+             sidebarLayout(
+               sidebarPanel(
+                 
+                  ),
+               
+               mainPanel(
+                
+               )
+             )#sidebar layout
+    ),
+    
+    #end of main panel 3 ####################################
+    ##########################################################
+    tabPanel("Residuals",
+             titlePanel("Residuals"),
+             sidebarLayout(
+               sidebarPanel(
+                 
+                
+               ),
+               
+               mainPanel(
+                 plotOutput('forecastPlot')
+                 
+               )
+             )#sidebar layout
     )
+    
+    #end of main panel 3 ####################################
+    ##########################################################
     
   )#end tabset panel
 )#end fluidpanel
